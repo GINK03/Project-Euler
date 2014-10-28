1c158d957d6a0fc6f1098a18bf0eb77f7bcde973
GINK03 <gim.kobayashi@gmail.com>
2014-10-28 14:50:08 +0900

    add 45 solution

---
+++ 45/euler.py
@@ -0,0 +1,36 @@
+import sys, itertools, math
+
+def isTriangle(h):
+    # fomula: 0 = n^2 + n - 2h
+    # h = (2n+1)^2 -1  //  8 
+    # if exist this Truee
+    for x in itertools.count(1):
+        fomula = ((x+1)*(x+1) - 1)/8
+        if h == fomula:
+            return True
+        if h < fomula:
+            return False
+            
+
+def isPentagonal(h):
+    # fomula: h = 3n^2 - n
+    # h = (6n-1)^2 -1  //  24
+    # if exist this Truee
+    for x in itertools.count(1):
+        fomula = ((6*x-1)*(6*x-1) - 1)/24
+        if h == fomula:
+            return True
+        if h < fomula:
+            return False
+
+def hexagonalGenerator():
+    g = 1
+    while True:
+        yield g*(2*g-1)
+        g += 1
+
+for h in hexagonalGenerator():
+    if isTriangle(h) and isPentagonal(h):
+        print h, isTriangle(h)
+        if 40755 < h:
+            break
