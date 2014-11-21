58fdfe19fa6e81a86d8f6b484e7b95dd17558b44
GINK03 <gim.kobayashi@gmail.com>
2014-11-21 21:45:43 +0900

    add 75 solution

--- 75/euler.py
+++ 75/euler.py
@@ -1,37 +1,18 @@
 import sys,os,math,itertools, operator, fractions
 sys.path.append('../pythonlib')
 import basic
-MAX = 10**6
 MAX = 1500000
-result = 0
-tgt = [x for x in xrange(1, MAX+1)]
-tgt.reverse()
-for x in tgt:
-    if x%100 == 0:
-        print 'iter', x
-    temp_res = 0
-    for m in xrange(1, int(math.sqrt(x/2)) + 1):
-        for n in xrange(1, m):
+temp_res = 0
+triangles = [0 for x in xrange(MAX+1)]
+for m in xrange(2, int(math.sqrt(MAX/2)) + 1):
+    for n in xrange(1, m+1):
+        if (m+n)%2 == 1 and fractions.gcd(n,m) == 1:
             a, b, c = m**2 - n**2, 2*m*n, m**2 + n**2
-            sum_entity = sum([a,b,c])
-            k = 1
-            if x%sum_entity == 0:
-                k = x/sum_entity
-                a, b, c = map(lambda x:x*k,[a, b, c])
-            sum_entity = sum([a,b,c])
-            if sum_entity == x:
-                #print 'heavy', m,n,k,a,b,c
-                temp_res += 1
-    '''
-    for m in xrange(1, int(math.sqrt(x/2)) + 1):   
-        n = x/(2*m) - m
-        a, b, c = m**2 - n**2, 2*m*n, m**2 + n**2
-        cflag = True
-        if sum([a,b,c]) == x:
-            print 'light', m, n, a, b, c
-            temp_res += 1
-    ''' 
-    if temp_res == 2 or temp_res == 1:
-        result += 1
-print 'result', result        
+            p = sum([a,b,c])
+            while p <= MAX:
+                triangles[p] += 1
+                if triangles[p] == 1: temp_res += 1
+                if triangles[p] == 2: temp_res -= 1
+                p += a+b+c
+print 'result', temp_res
 
