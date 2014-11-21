ae66fb489ab164269365fe5c05458fc05d71efe1
GINK03 <gim.kobayashi@gmail.com>
2014-11-21 23:28:57 +0900

    add solution 2 with nodejs

---
+++ 2/euler.js
@@ -0,0 +1,22 @@
+
+
+var MAX = 4000000
+
+var fib = {
+  head: 1,
+  tail: 0,
+  inc: function(){
+    var temp = this.head;
+    this.head = this.head + this.tail;
+    this.tail = temp;
+  },
+}
+
+var result = 0;
+while(true){
+  //console.log(fib.head, fib.tail);
+  if(fib.head > MAX) break;
+  if(fib.head%2 == 0) result += fib.head;
+  fib.inc();
+}
+console.log('result,', result);
