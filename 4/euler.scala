24a4cb9787a2d3d77c51c85175e4f673b2743ca2
GINK03 <gim.kobayashi@gmail.com>
2014-11-22 12:39:11 +0900

    add solution 4 with scala

--- 4/euler.scala
+++ 4/euler.scala
@@ -1,28 +1,29 @@
 
 
 
-
-
-
 object main{
-  def check(line: String){
+  def check(line: String):Boolean = {
     val carray  = line.toCharArray
     val len     = carray.length/2
     val heads   = carray.take(len)
-    val tail    = carray.takeRight(len)
-    println(heads.mkString + " " + tail.mkString)
-    return true
+    val tail    = carray.takeRight(len).reverse
+    if(heads.mkString == tail.mkString)
+      return true
+    else
+      return false
   }
-  var source = List[Long](1,2,3)
+  var source = List[Long]()
+  var result = List[Long]()
   def main(args: Array[String]){
     for(i <- 100 to 999){
       source = i :: source
     }
     source.combinations(2).foreach( e => {
       val raw = e.reduce(_ * _).toString()
-      check(raw)
-      println(raw)
+      if( check(raw) ){
+        result = raw.toInt :: result
+      }
     })
-    println("Hello World")
+    println("ans," + result.sorted.takeRight(1).toString())
   }
 }
